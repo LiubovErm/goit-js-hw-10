feec3a15228441ff3e9c947900b72c3442ed5f1e
LiubovErm <ermakovaluba27@gmail.com>
2022-09-14 14:11:16 +0300

    fix fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,33 @@
+import Notiflix from 'notiflix';
+
 function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1/name';
   const FILTERS = '?fields=name,capital,flags,languages,population';
 
-  return fetch(`${BASE_URL}/${name}${FILTERS}`)
-    .then(response => response.json())
-    .catch(error => {console.log(error);
+return fetch(`${BASE_URL}/${name}${FILTERS}`)
+.then(response => {
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  if (response.status === 404) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  }
 });
-}
+};
 
 export { fetchCountries };
+
+
+
+// function fetchCountries(name) {
+//   const BASE_URL = 'https://restcountries.com/v3.1/name';
+//   const FILTERS = '?fields=name,capital,flags,languages,population';
+
+//   return fetch(`${BASE_URL}/${name}${FILTERS}`)
+//     .then(response => response.json())
+//     .catch(error => {console.log(error);
+// });
+// }
+
+// export { fetchCountries };
